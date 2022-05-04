2af00c20ec0d0e03456ea3bf6493c8bfcd809297
Jeron <norejudeisa@gmail.com>
2022-05-04 10:57:27 +0000

    fetched category from mealdb

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -11,41 +11,42 @@ import {
   Center,
   useMediaQuery,
 } from '@chakra-ui/react'
-import { useRef, useCallback } from 'react'
+import { useRef, useCallback, useEffect, useState, Suspense } from 'react'
 import { BiLeftArrow, BiRightArrow } from 'react-icons/all'
 import { Swiper, SwiperSlide } from 'swiper/react'
 const Menu = () => {
   const [tablet] = useMediaQuery('(min-width:62em)')
   const [desktop] = useMediaQuery('(min-width:80em')
   const sliderRef = useRef()
+  const [menu, setMenu] = useState([])
   let spacing = tablet ? 15 : desktop ? 20 : 10
 
-  const menus = [
-    {
-      name: 'Salad',
-      icon: '/images/salad.svg',
-    },
-    {
-      name: 'Burger',
-      icon: '/images/burger.svg',
-    },
-    {
-      name: 'Chicken',
-      icon: '/images/chicken.svg',
-    },
-    {
-      name: 'Drink',
-      icon: '/images/drink.svg',
-    },
-    {
-      name: 'Chicken',
-      icon: '/images/chicken.svg',
-    },
-    {
-      name: 'Drink',
-      icon: '/images/drink.svg',
-    },
-  ]
+  // const menus = [
+  //   {
+  //     name: 'Salad',
+  //     icon: '/images/salad.svg',
+  //   },
+  //   {
+  //     name: 'Burger',
+  //     icon: '/images/burger.svg',
+  //   },
+  //   {
+  //     name: 'Chicken',
+  //     icon: '/images/chicken.svg',
+  //   },
+  //   {
+  //     name: 'Drink',
+  //     icon: '/images/drink.svg',
+  //   },
+  //   {
+  //     name: 'Chicken',
+  //     icon: '/images/chicken.svg',
+  //   },
+  //   {
+  //     name: 'Drink',
+  //     icon: '/images/drink.svg',
+  //   },
+  // ]
   const handlePrev = useCallback(() => {
     if (!sliderRef.current) return
     sliderRef.current.swiper.slidePrev()
@@ -55,6 +56,17 @@ const Menu = () => {
     if (!sliderRef.current) return
     sliderRef.current.swiper.slideNext()
   }, [])
+  const fetchMenu = async () => {
+    const response = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/categories.php'
+    )
+    const { categories } = await response.json()
+    setMenu(categories)
+  }
+  useEffect(() => {
+    fetchMenu()
+  }, [])
+
   return (
     <VStack alignItems="flex-start" py={8} spacing={8}>
       <HStack w="full">
@@ -83,62 +95,70 @@ const Menu = () => {
           <IconButton icon={<BiRightArrow size={20} />} onClick={handleNext} />
         </HStack>
       </HStack>
-      <Swiper
-        spaceBetween={spacing}
-        slidesPerView={3}
-        grabCursor
-        style={{
-          width: '100%',
-        }}
-        loop
-        breakpoints={{
-          768: {
-            slidesPerView: 5,
-          },
-          992: {
-            slidesPerView: 7,
-          },
-        }}
-        ref={sliderRef}
-      >
-        {menus.map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <LinkBox>
-              <VStack
-                p={6}
-                bg="blue.100"
-                rounded="md"
-                _hover={{
-                  bg: 'blue.400',
-                }}
-                role="group"
-                transition="0.5s ease background-color"
-              >
-                <Center bg="white" w={20} p={3} rounded="full">
-                  <LinkOverlay href="/">
+      <Suspense fallback={<Text>Loading</Text>}>
+        <Swiper
+          spaceBetween={spacing}
+          slidesPerView={2}
+          grabCursor
+          style={{
+            width: '100%',
+          }}
+          loop
+          breakpoints={{
+            768: {
+              slidesPerView: 5,
+            },
+            992: {
+              slidesPerView: 7,
+            },
+          }}
+          ref={sliderRef}
+        >
+          {menu.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <LinkBox>
+                <VStack
+                  p={6}
+                  bg="blue.100"
+                  rounded="md"
+                  _hover={{
+                    bg: 'blue.400',
+                  }}
+                  role="group"
+                  transition="0.5s ease background-color"
+                >
+                  <Center
+                    // bg="white"
+                    w={40}
+                    // h={30}
+                    // rounded="full"
+                    overflow="hidden"
+                  >
+                    {/* <LinkOverlay href="/"> */}
                     <Image
-                      src={item.icon}
-                      alt={item.name}
+                      src={item.strCategoryThumb}
+                      alt={item.strCategory}
                       w="full"
                       h="full"
                       objectFit="cover"
                     />
-                  </LinkOverlay>
-                </Center>
-                <Text
-                  _groupHover={{
-                    color: 'white',
-                  }}
-                  transition="0.5s ease"
-                  fontSize="lg"
-                >
-                  {item.name}
-                </Text>
-              </VStack>
-            </LinkBox>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+                    {/* </LinkOverlay> */}
+                  </Center>
+                  <Text
+                    _groupHover={{
+                      color: 'white',
+                    }}
+                    transition="0.5s ease"
+                    fontSize="lg"
+                  >
+                    {item.strCategory}
+                  </Text>
+                </VStack>
+              </LinkBox>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Suspense>
     </VStack>
   )
 }
