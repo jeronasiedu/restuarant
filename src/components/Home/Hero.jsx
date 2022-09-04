0844ccc5e657018193de0106ea16979d2a3098c8
Jeron <norejudeisa@gmail.com>
2022-09-04 21:54:30 +0000

    change font family

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -10,26 +10,26 @@ import {
   Input,
   chakra,
   InputLeftElement,
-} from '@chakra-ui/react'
-import { GiMeal } from 'react-icons/all'
-import heroImage from '../../assets/asset2.png'
+} from "@chakra-ui/react";
+import { GiMeal } from "react-icons/all";
+import heroImage from "../../assets/asset2.png";
 const Hero = () => {
   return (
-    <Stack py={16} direction={['column', 'column', 'row']} spacing={[8, 5, 3]}>
+    <Stack py={16} direction={["column", "column", "row"]} spacing={[8, 5, 3]}>
       <VStack flex="1" spacing={8}>
-        <Heading size="3xl" textAlign={['center', 'center', 'left']}>
+        <Heading size="3xl" textAlign={["center", "center", "left"]}>
           It's not just food, it's an experience
         </Heading>
         <Text
           fontSize="xl"
-          textAlign={['center', 'center', 'left']}
+          textAlign={["center", "center", "left"]}
           color="gray.600"
         >
           Hey! Our delicious food is waiting for you, we are always near to you
           with fresh item of food
         </Text>
         <chakra.form
-          alignSelf={['center', 'center', 'flex-start']}
+          alignSelf={["center", "center", "flex-start"]}
           color="gray.300"
         >
           <InputGroup>
@@ -55,7 +55,7 @@ const Hero = () => {
       </VStack>
       <Center flex="1">
         <Image
-          src={heroImage}
+          src="/images/food2.png"
           alt="An image of food on a plate"
           w="full"
           h="full"
@@ -63,7 +63,7 @@ const Hero = () => {
         />
       </Center>
     </Stack>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
-import '@fontsource/oswald'
-import 'swiper/css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/oswald";
+import "swiper/css";
+import { extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+const theme = extendTheme({
+  fonts: {
+    heading: `'capriola', sans-serif`,
+    body: `'fira mono', sans-serif`,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </ChakraProvider>
-)
+);
