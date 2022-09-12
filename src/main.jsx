4053c188214252ac19ab08c5dc3fcadcdfbb738f
Jeron <norejudeisa@gmail.com>
2022-09-12 20:54:10 +0000

    implement swiper card effect

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
 import { ChakraProvider } from "@chakra-ui/react"
 import "@fontsource/oswald"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import "swiper/css"
+import "swiper/css/effect-cards"
+import App from "./App"
+import "./index.css"
 import theme from "./theme/appTheme"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
