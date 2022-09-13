8f5c51e4ff9f971e6779c8282f5b3a9bd3b9ee2d
Jeron <norejudeisa@gmail.com>
2022-09-13 15:49:13 +0000

    reduce padding on about section

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
     <Stack
       w="full"
       py={14}
-      px={5}
+      px={2}
       spacing={[8, 12, 16, 24]}
       direction={["column-reverse", "column-reverse", "column-reverse", "row"]}
     >
